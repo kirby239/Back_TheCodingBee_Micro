55acf036234656556a670030d587334f58918c27
kirby239 <thumastercito428@gmail.com>
2023-01-07 23:41:55 -0500

    subiendo mi primer microservice

---
+++ config-server/src/test/java/com/empresa/pe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.pe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
